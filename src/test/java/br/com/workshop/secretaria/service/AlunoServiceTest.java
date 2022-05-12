b0025b103b03cfc2d3a8a8c684c3a5dc6ea805ba
LeviQFreitas <Levi.Freitas@globalweb.com.br>
2022-05-12 13:56:48 -0300

    "Implementando testes de delete e getAll"

--- src/test/java/br/com/workshop/secretaria/service/AlunoServiceTest.java
+++ src/test/java/br/com/workshop/secretaria/service/AlunoServiceTest.java
@@ -3,17 +3,45 @@ package br.com.workshop.secretaria.service;
 import br.com.workshop.secretaria.AplicationConfigTest;
 import br.com.workshop.secretaria.domain.Aluno;
 import br.com.workshop.secretaria.repository.AlunoRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.anyLong;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class AlunoServiceTest extends AplicationConfigTest {
+class AlunoServiceTest {
+
+    public static final Long MATRICULA   = 1L;
+    public static final String ESCOLA    = "Objetivo";
+    public static final String NOME      = "Daiane";
+    public static final LocalDate DATA_NASCIMENTO = LocalDate.of(2002,
+            2, 20);
+    public static final String SERIE     = "3A-EM";
+    public static final int INDEX = 0;
+
+    @BeforeEach
+        // antes realiza um trecho de código
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startAluno();
+    }
+    @Mock
+    private List<Aluno> listAluno;
+
+    @Mock
+    private Aluno aluno;
+
+    @Mock
+    private Aluno aluno2;
 
     @Mock
     private AlunoRepository repository;
@@ -43,4 +71,37 @@ class AlunoServiceTest extends AplicationConfigTest {
         assertEquals(aluno.getSerie(), alunoCreated.getSerie());
         assertEquals(aluno.getDataNascimento(), alunoCreated.getDataNascimento());
     }
+
+    @Test
+    void testDeleteAluno() {
+        doNothing().when(repository).deleteById(anyLong());
+        service.deleteAluno(MATRICULA);
+        verify(repository, times(1)).deleteById(anyLong());
+    }
+
+
+    @Test
+    void getAll() {
+
+        when(repository.findAll()).thenReturn(listAluno);
+        List<Aluno> alunoList = service.getAll();
+        assertNotNull(alunoList.get(INDEX));
+    }
+
+    private void startAluno() {
+
+         aluno = new Aluno(MATRICULA, ESCOLA,
+                NOME, DATA_NASCIMENTO, SERIE);
+         aluno2 = new Aluno(2L, "Objetivo",
+                 "Pedro", LocalDate.of(2000,10,2), "3A - EM");
+
+         listAluno = List.of(aluno,aluno2);
+
+    }
+
+
+
+
+
+
 }
